bc72efed92c3fc1f4493bcc825adcae7e383e5a3
Jonathan <jon.lundholm@gmail.com>
2022-04-06 22:33:04 +0200

    chore: simplify logging init

--- src/logging.rs
+++ src/logging.rs
@@ -1,9 +1,6 @@
-use crate::color;
 use anyhow::{bail, Result};
-use env_logger::fmt::Formatter;
 use env_logger::Builder;
-use log::{LevelFilter, Record};
-use std::io::Write;
+use log::LevelFilter;
 
 /// Initialize logging.
 pub fn init(level: &str) -> Result<()> {
@@ -16,25 +13,6 @@ pub fn init(level: &str) -> Result<()> {
         s => bail!("invalid log level: {}", s),
     };
 
-    let mut builder = Builder::new();
-
-    // Do not show any logs from these modules with level lower than warning
-    // for m in &[""] {
-    //     builder.filter_module(m, LevelFilter::Warn);
-    // }
-
-    builder.filter_level(level).format(format).init();
+    Builder::new().filter(Some("dotf"), level).init();
     Ok(())
-}
-
-fn format(f: &mut Formatter, r: &Record) -> std::io::Result<()> {
-    let level = r.level();
-    let clr = match level {
-        log::Level::Error => color::RED,
-        log::Level::Warn => color::YELLOW,
-        log::Level::Info => color::GREEN,
-        log::Level::Debug => color::BLUE,
-        log::Level::Trace => color::CYAN,
-    };
-    writeln!(f, "{}{:^5}{} - {}", clr, level, color::RESET, r.args())
 }
